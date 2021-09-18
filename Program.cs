3b0eb81eb7ec3615e2d2790d5e3d6914b5086edb
souravporel <souravporel.2010@gmail.com>
2021-09-18 13:35:27 +0530

    1st Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreCSharp_Concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
